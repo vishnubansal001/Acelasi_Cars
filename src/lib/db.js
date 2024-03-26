c1cceb5d037f98c6b6d01d61683a2b66164197fb
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2024-03-26 21:51:47 +0530

    added the db connection

---
+++ src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+export const connectToDB = async () => {
+  if (!mongoose.connections[0].readyState) {
+    try {
+      const con = await mongoose.connect(process.env.MONGO_URL, {
+        dbName: process.env.PRODUCTION == "true" ? "main" : "test",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`MongoDB is Connected with Host: ${con.connection.host}`);
+    } catch (error) {
+      console.log("Error connecting to mongo.", error);
+    }
+  }
+};
