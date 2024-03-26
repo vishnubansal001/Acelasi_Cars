a6c6a23e326db51b94be8ad1117338795f937ef1
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2024-03-26 21:48:40 +0530

    added the models

---
+++ src/app/_models/car.model.js
@@ -0,0 +1,124 @@
+import mongoose from "mongoose";
+
+const carSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  make: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  ownerName: {
+    type: String,
+    required: true,
+  },
+  ownerEmail: {
+    type: String,
+    required: true,
+  },
+  ownerPhone: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  mileage: {
+    type: Number,
+    required: true,
+  },
+  bodyType: {
+    type: String,
+    enum: [
+      "Sedan",
+      "Coupe",
+      "SUV",
+      "Truck",
+      "Van",
+      "Wagon",
+      "Convertible",
+      "Hatchback",
+      "Other",
+    ],
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+    enum: [
+      "Black",
+      "White",
+      "Silver",
+      "Grey",
+      "Red",
+      "Blue",
+      "Brown",
+      "Green",
+      "Yellow",
+      "Orange",
+      "Purple",
+      "Other",
+    ],
+  },
+  engine: {
+    type: String,
+    required: true,
+    enum: ["V4", "V6", "V8", "V12", "Other"],
+  },
+  transmission: {
+    type: String,
+    required: true,
+    enum: ["Automatic", "Manual", "Other"],
+  },
+  driveTrain: {
+    type: String,
+    required: true,
+    enum: ["FWD", "RWD", "AWD", "4WD", "Other"],
+  },
+  fuelType: {
+    type: String,
+    required: true,
+    enum: ["Gasoline", "Diesel", "Electric", "Hybrid", "Other"],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  features: {
+    type: [String],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  report: {
+    type: String,
+  },
+});
+
+const Car = mongoose.models["Car"] || mongoose.model("Car", carSchema);
+
+export default Car;
---
+++ src/app/_models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+const User = mongoose.models["User"] || mongoose.model("User", userSchema);
+
+export default User;
